31c907f29a8a1feb065d92ade59b7a7d8e303c68
jespergran98 <jesper25sfj@gmail.com>
2025-09-23 09:08:00 +0200

    update

--- obj/Debug/net9.0/FrontendToBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/FrontendToBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontendToBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e134a6b630b0c472a733d693b02862a9bfbc6fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontendToBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontendToBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
