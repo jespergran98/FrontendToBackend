b98490f6a354cca6e8bf5fa9b2b0e373fb871b1b
jespergran98 <jesper25sfj@gmail.com>
2025-09-23 12:21:17 +0200

    working again

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure middleware
--- obj/Debug/net9.0/FrontendToBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/FrontendToBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontendToBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02db920cda19f9ccae1dff7a2c084ab8af02b8d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c398f290d5be65025361ee311235aa8e75a6c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontendToBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontendToBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
